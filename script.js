3b34aea08bc4bd138736a53d95c4aa3814e6967f
charlieqcao <charlieqcao@outlook.com>
2021-04-04 14:54:01 -0700

    update nav bar to mimic ZARA website

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 /* navbar */
-function myFunction() {
-  var x = document.getElementById("navbar-custom");
-  if (x.className === "navbar-custom") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar-custom";
-  }
-}
+const navBurger = document.querySelector('.navbar-burger');
+const navCross = document.querySelector('.navbar-cross');
+const nav = document.querySelector('.navbar-menu');
+
+navBurger.addEventListener('click', function() {
+  toggleNav();
+});
+navCross.addEventListener('click', function() {
+  toggleNav();
+});
+
+function toggleNav() {
+  nav.classList.toggle('nav-active');
+  navBurger.classList.toggle('nav-hide')
+};
